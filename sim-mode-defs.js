02dec000e210e9f1f840ee238dcdf6ef81eb0b1d
StormCoderWorld <131701367+StormCoderWorld@users.noreply.github.com>
2024-03-27 13:20:43 -0400

    Update sim-mode-defs.js

--- sim-mode-defs.js
+++ sim-mode-defs.js
@@ -1625,7 +1625,7 @@ STORM_ALGORITHM.defaults.core = function(sys,u){
     let nontropicalness = constrain(map(sys.lowerWarmCore,0.75,0,0,1),0,1);
 
     sys.organization *= 100;
-    if(!lnd) sys.organization += sq(map(SST,20,28,30,0,1,2,true))*3*tropicalness;
+    if(!lnd) sys.organization += sq(map(SST,20,28,30,0,0.5,1,true))*3*tropicalness;
     if(!lnd && sys.organization<40) sys.organization += lerp(0,3,nontropicalness);
     // if(lnd) sys.organization -= pow(10,map(lnd,0.5,1,-3,1));
     // if(lnd && sys.organization<70 && moisture>0.3) sys.organization += pow(5,map(moisture,0.3,0.5,-1,1,true))*tropicalness;
