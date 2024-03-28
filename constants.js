d5e1fadc6b28e083a652f5ef5988d0914702fe72
StormCoderWorld <131701367+StormCoderWorld@users.noreply.github.com>
2024-03-28 15:30:05 -0400

    Update constants.js
    Updated version number

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
 const TITLE = "Cyclone Simulator";
-const VERSION_NUMBER = "0.5.8";
+const VERSION_NUMBER = "0.5.9";
 const SAVE_FORMAT = 7;  // Format #7 in use starting in v0.4
 const EARLIEST_COMPATIBLE_FORMAT = 0;
 const ENVDATA_COMPATIBLE_FORMAT = 0;
