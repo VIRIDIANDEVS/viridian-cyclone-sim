f6b835752a1dd4ce5fd220d2288658af0184291e
StormCoderWorld <131701367+StormCoderWorld@users.noreply.github.com>
2024-04-03 22:45:44 -0400

    Update ui.js

--- ui.js
+++ ui.js
@@ -427,7 +427,7 @@ UI.init = function(){
         text(TITLE,0,0);
         textSize(18);
         textStyle(ITALIC);
-        text("Simulate your own monster storms!",0,40);
+        text("Simulate tropical cyclones from birth to death!",0,40);
     });
 
     mainMenu.append(false,WIDTH/2-100,HEIGHT/2-20,200,40,function(s){    // "New Basin" button
