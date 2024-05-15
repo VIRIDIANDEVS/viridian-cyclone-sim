45bf843bd0da37cf75efc7671a03dbc28886fa35
StormCoderWorld <131701367+StormCoderWorld@users.noreply.github.com>
2024-05-15 16:35:24 -0400

    Update sim-mode-defs.js

--- sim-mode-defs.js
+++ sim-mode-defs.js
@@ -1053,7 +1053,7 @@ ENV_DEFS.defaults.SST = {
         if(y<0) return 0;
         let anom = u.field('SSTAnomaly');
         let s = seasonalSine(z);
-        let w = map(cos(map(x,0,WIDTH, -PI, 2*PI)),-1,1,0.30,0.70);
+        let w = map(cos(map(x,0,WIDTH, -1*PI, 2*PI)),-1,1,0.30,0.70);
         let h0 = y/HEIGHT;
         let h1 = (sqrt(h0)+h0)/2;
         let h2 = sqrt(sqrt(h0));
