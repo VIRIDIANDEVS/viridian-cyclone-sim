4b23a478304322ec59342417ea9a7890a6a3db7c
StormCoderWorld <131701367+StormCoderWorld@users.noreply.github.com>
2024-06-10 00:55:28 -0400

    Update sim-mode-defs.js
    Fixed the pressure calculation, made land height affect organization, tropical waves should no longer die over land that is not inhospitable.

--- sim-mode-defs.js
+++ sim-mode-defs.js
@@ -1702,10 +1702,10 @@ STORM_ALGORITHM.defaults.core = function(sys,u){
 
     sys.organization *= 100;
     if(!lnd && moisture >= 0.8) {
-        sys.organization += sq(map(SST,20,26,28,29.5,31,0,0.2,0.5,0.8,1,true))*3*tropicalness;
+        sys.organization += sq(map(SST,20,26,28,29.5,31,0,0.14,0.3,0.6,1,true))*3*tropicalness;
     }
 else if (!lnd && moisture >= 0.7 && moisture < 0.8) {
-sys.organization += sq(map(SST,20,26,28,29.5,31,0,0.1,0.2,0.4,0.6,true))*3*tropicalness;
+sys.organization += sq(map(SST,20,26,28,29.5,31,0,0.07,0.15,0.3,0.5,true))*3*tropicalness;
 }
 
 else if (!lnd && moisture >= 0.6 && moisture < 0.7) {
@@ -1720,9 +1720,12 @@ sys.organization += sq(map(SST,20,26,28,29.5,31,0,0.001,0.005,0.01,0.07,true))*3
     if(!lnd && sys.organization<40) sys.organization += lerp(0,3,nontropicalness);
     // if(lnd) sys.organization -= pow(10,map(lnd,0.5,1,-3,1));
     // if(lnd && sys.organization<70 && moisture>0.3) sys.organization += pow(5,map(moisture,0.3,0.5,-1,1,true))*tropicalness;
-if(lnd < 0.65 && sys.organization<5 && moisture >= 0.5) {
-sys.pressure !>= 1013)
+if (lnd < 0.65 && sys.organization < 5 && moisture >= 0.5) {
+    if (!(sys.pressure >= 1013)) {
+        sys.pressure <= 1012; // Or any value below 1013 that you want to set it to
+    }
 }
+
 
     sys.organization -= pow(2,4-((HEIGHT-sys.basin.hemY(sys.pos.y))/(HEIGHT*0.01)));
     sys.organization -= (pow(map(sys.depth,0,1,1.17,1.31),shear)-1)*map(sys.depth,0,1,4.7,1.2);
